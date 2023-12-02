a81decbf7d6671c2e7ecdb2530ef5045e4897868
Alex Hek <alexanderhek@gmail.com>
2023-12-02 15:21:01 +0000

    Update FirebaseApp.svelte, made props all optional

--- src/lib/components/FirebaseApp.svelte
+++ src/lib/components/FirebaseApp.svelte
@@ -4,9 +4,9 @@
   import type { Firestore } from "firebase/firestore";
   import type { FirebaseStorage } from "firebase/storage";
 
-  export let firestore: Firestore;
-  export let auth: Auth;
-  export let storage: FirebaseStorage;
+  export let firestore: Firestore | undefined = undefined;
+  export let auth: Auth | undefined = undefined;
+  export let storage: FirebaseStorage | undefined = undefined;
 
   setFirebaseContext({ firestore, auth, storage });
 </script>
